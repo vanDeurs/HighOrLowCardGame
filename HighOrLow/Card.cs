160105bf29bb7c43d906297f0c0c10413b9147b8
Alexander <alexwvd@outlook.com>
2019-04-10 15:32:15 +0200

    init

---
+++ HighOrLow/Card.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HighOrLow
+{
+    class Card
+    {
+        // Hjärter, Spader, Ruter, Klöver
+        public string färg;
+        // 1 (Ess) - 14 (Ess)
+        public int värde;
+        // Returnerar kortets värde
+        public int ReturnernaVärde(Card card)
+        {
+            return card.värde;
+        }
+    }
+}
--- HighOrLow/Program.cs
+++ HighOrLow/Program.cs
@@ -10,6 +10,47 @@ namespace HighOrLow
     {
         static void Main(string[] args)
         {
+            IList<Card> DeckOfCards = InitieraKortlek();
+            foreach (var card in DeckOfCards)
+            {
+                Console.WriteLine(card.färg + " " + card.värde);
+            }
+            Console.ReadKey();
+        }
+        static IList<Card> InitieraKortlek ()
+        {
+            IList<Card> DeckOfCards = new List<Card>() { };
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 12; j++)
+                {
+                    switch (i)
+                    {
+                        case 0:
+                            DeckOfCards.Add(new Card() { färg = "hjärter", värde = (j + 1) });
+                            break;
+                    }
+                    switch (i)
+                    {
+                        case 1:
+                            DeckOfCards.Add(new Card() { färg = "ruter", värde = (j + 1) });
+                            break;
+                    }
+                    switch (i)
+                    {
+                        case 2:
+                            DeckOfCards.Add(new Card() { färg = "spader", värde = (j + 1) });
+                            break;
+                    }
+                    switch (i)
+                    {
+                        case 3:
+                            DeckOfCards.Add(new Card() { färg = "klöver", värde = (j + 1) });
+                            break;
+                    }
+                }
+            }
+            return DeckOfCards;
         }
     }
 }
