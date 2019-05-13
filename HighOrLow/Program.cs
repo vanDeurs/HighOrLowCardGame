9d755540a82ef7b8e58cdc0455b77c5cde9d8754
Alexander <alexwvd@outlook.com>
2019-05-13 18:17:40 +0200

    game is done!!

--- HighOrLow/Program.cs
+++ HighOrLow/Program.cs
@@ -6,61 +6,79 @@ using System.Threading.Tasks;
 
 namespace HighOrLow
 {
-    // TODO: Enum för färger
     class Program
     {
         static void Main(string[] args)
         {
+            // Skriv ut reglerna första gången använder startar spelet
+            Console.WriteLine("**********************************************************************************************");
+            Console.WriteLine("Välkommen till spelet High or Low!");
+            Console.WriteLine("Regler: 13 kort av 52 läggs med baksidan upp. Sedan vänds det första kortet.");
+            Console.WriteLine("Ditt jobb är att avgöra om nästa kort är högre eller " +
+            "lägre tills alla 13 kort är passerade.\nKlarar du alla gissningar vinner du. Gissar du fel förlorar du.\nEss " +
+            "fungerar som trumf, d.v.s att det är både högst och lägst. Du förlorar dock om det blir par.\nLycka till!");
+            Console.WriteLine("**********************************************************************************************");
+
+            // Starta spel-loopen
+            MainLoop();
+        }
+
+        static void MainLoop ()
+        {
+            // Skapa kortlek
             IList<Card> deckOfCards = InitieraKortlek();
+
+            // Generera 13 random kort från kortleken
             IList<Card> randomCards = Generera13RandomKort(deckOfCards);
 
+            // Loopen som håller spelet i liv
             while (true)
             {
                 try
                 {
-                   // Generera 13 random kort från kortleke 
-                   StartGameLoop(randomCards);
+                    StartGame(randomCards);
                 }
                 catch (Exception error)
                 {
                     Console.WriteLine(error.Message);
-                    Console.WriteLine("Vill du köra igen eller avsluta?");
+                    Console.WriteLine("Om du vill köra igen klicka 1: ");
                     Console.WriteLine("1. Kör igen");
-                    Console.WriteLine("2. Avsluta");
+                    Console.WriteLine("Annan. Avsluta");
 
-                    while (true)
-                    {
-                        try
-                        {
-                            ConsoleKeyInfo svar = Console.ReadKey(true);
-                            string svarString = svar.KeyChar.ToString();
-                            int svarInt = int.Parse(svarString);
-                            Console.WriteLine(svarInt);
-                        }
-                        catch (Exception error2)
-                        {
-                            Console.WriteLine(error2.Message);
-                            Console.WriteLine("Felaktig input.");
-                        }
-
-                    }
+                    // Hanterar vad användaren vill göra vid slut
+                    EndGameHandler();
                 }
             }
         }
 
-        static void StartGameLoop(IList<Card> randomCards) {
-            Console.WriteLine("Välkommen till High or Low. Spelet fungerar så att 13 kort, av 52, " +
-                "läggs ut med baksidan upp, sen vänds det första kortet. Ditt jobb är att avgöra om nästa kort är högre eller " +
-                "lägre tills alla 13 kort är passerade. Klarar du alla gissningar vinner du. Gissar du fel förlorar du. Ess " +
-                "fungerar som trumf d v s det är både högst och lägst. Du förlorar dock om det blir par. Lycka till!");
+        // Hanterar slutet på spelet
+        static void EndGameHandler ()
+        {
+            ConsoleKeyInfo svar = Console.ReadKey(true);
+            string svarString = svar.KeyChar.ToString();
+            int svarInt = int.Parse(svarString);
+
+            if (svarInt == 1)
+            {
+                // Startar om spelet
+                MainLoop();
+            } else
+            {
+                // Eller stänger av det
+                Environment.Exit(0);
+            }
+        }
+
+        static void StartGame(IList<Card> randomCards) {
             Console.WriteLine("");
+
+            // Skriver ut de kort som hitills har vänt på
             for (int j = 0; j < 13; j++)
             {
                 for (int i = 0; i < 13; i++)
                 {
                     if (i == j || i < j)
                     {
-                        // TODO: Avgör om kort är en färg
                         Console.Write(" [" + (Färger)randomCards[i].färg + " " + (Värden)randomCards[i].värde + "] ");
                     }
                     else
@@ -69,34 +87,35 @@ namespace HighOrLow
                     }
                 }
                 Console.WriteLine("");
+
+                // Här får spelaren välja högre eller lägre
                 int spelarensVal = PlayTurn(randomCards[j]);
+
+                // Här ser vi om spelaren valde rätt eller fel
                 switch (spelarensVal)
                 {
                     case 1:
                         // Om nästa kort i listen är av ett högre värde än det nuvarande
-                        if (randomCards[j].värde == 1 || randomCards[j + 1].värde > randomCards[j].värde)
+                        if (randomCards[j].värde == 1 || randomCards[j + 1].värde == 1 || randomCards[j + 1].värde > randomCards[j].värde)
                         {
-                            Console.WriteLine("");
-                            Console.WriteLine("Rätt!");
-                            Console.WriteLine("");
                             break;
                         }
                         Console.WriteLine("Fel! Kortet som vänds är {0}", (Färger)randomCards[j+1].färg + " " + (Värden)randomCards[j+1].värde);
                         throw new Exception("");
                     // Om nästa kort i listen är av ett mindre värde än det nuvarande
                     case 2:
-                        if (randomCards[j + 1].värde < randomCards[j].värde)
+                        if (randomCards[j].värde == 1 || randomCards[j + 1].värde == 1 || randomCards[j + 1].värde < randomCards[j].värde)
                         {
-                            Console.WriteLine("Yes, du har rätt!");
                             break;
                         }
                         Console.WriteLine("Fel! Kortet som vänds är {0}", (Färger)randomCards[j + 1].färg + " " + (Värden)randomCards[j + 1].värde);
                         throw new Exception("");
-                    default:
-                        Console.WriteLine("Fel! Kortet som vänds är {0}", (Färger)randomCards[j + 1].färg + " " + (Värden)randomCards[j + 1].värde);
-                        throw new Exception("");
                 }
-
+                // Om spelaren har rätt kommer switchen breaka och komma hit
+                // Annars kommer den hamna i en Exception
+                Console.WriteLine("");
+                Console.WriteLine(" Rätt!");
+                Console.WriteLine("");
             }
         }
 
@@ -108,7 +127,7 @@ namespace HighOrLow
                 try
                 {
                     Console.WriteLine("");
-                    Console.WriteLine(" Kortet som vänds är {0}", (Färger)kort.färg + " " + (Värden)kort.värde);
+                    Console.WriteLine(" Är nästa kort högre eller lägre än {0}?", (Färger)kort.färg + " " + (Värden)kort.värde);
                     Console.WriteLine("");
                     Console.WriteLine("1. Högre");
                     Console.WriteLine("2. Lägre");
@@ -129,12 +148,9 @@ namespace HighOrLow
                     Console.WriteLine(error.Message);
                 }
             }
-
-
         }
 
-        // Initiera kortleken, dvs generera 13 kort av varje färg och spara dem i en list
-        // som returnas
+        // Genererar en full kortlek
         static IList<Card> InitieraKortlek()
         {
             IList<Card> deckOfCards = new List<Card>() { };
@@ -147,7 +163,7 @@ namespace HighOrLow
             }
             return deckOfCards;
         }
-        // Generera 13 random kort utifrån kortleken, spara dem i en list
+        // Generera 13 random kort utifrån kortleken, spara dem i en lista
         // och return dem
         static IList<Card> Generera13RandomKort(IList<Card> kortlek)
         {
